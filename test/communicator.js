f245a35866276f1d13829fa7839ecb37e9e22e48
troyth <troyth@gmail.com>
2013-09-15 16:40:25 -0400

    added grunt tests

---
+++ test/communicator.js
@@ -0,0 +1,9 @@
+var home = require("../lib/phone-home")
+	, machine = require('../machine');//machine.json
+
+
+exports["Communicator"] = function(test){
+    test.expect(1);
+    test.ok(true, "this assertion should pass");
+    test.done();
+};
