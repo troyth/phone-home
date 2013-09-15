f3e073cb47c6b9cf561fe4e4fe91edf194c4f468
troyth <troyth@gmail.com>
2013-09-15 09:05:12 -0400

    added example app

---
+++ test/app.js
@@ -0,0 +1,48 @@
+var phone_home = require("phone-home");
+
+/**
+*
+* Initialize all sensors and actuators in machine.json
+* 
+* Uses machine.json to load handlers (eg. Johnny-Five, node-raspicam, etc)
+* and initialize listeners and create objects (eg. sensors, cameras)
+*
+* Triggers "ready" event on phone_home when complete
+*
+**/
+phone_home.init();
+
+
+/**
+*
+* Begin the app
+*
+**/
+phone_home.on("ready", function(){
+
+	/**
+	*
+	* Additional setup and bindings for each sensor and actuator
+	*
+	**/
+	//cycle through inputs to bind listeners and make changes individually
+	for(var in in phone_home.inputs){
+
+	}
+
+	//cycle through outputs to bind listeners and make changes individually
+	for(var out in phone_home.outputs){
+
+	}
+
+
+	/**
+	*
+	* Start sending reports to the server and driving actuators based on logic
+	* above and signals from the server
+	*
+	**/
+	phone_home.start();
+
+
+});
